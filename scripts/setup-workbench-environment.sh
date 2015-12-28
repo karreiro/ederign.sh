fc9b5e97158f7deb3e522702c5765e855453c9a2
Paulo Martins <paulovmr@gmail.com>
2015-12-28 13:17:46 -0200

    Logging on scripts

--- scripts/setup-workbench-environment.sh
+++ scripts/setup-workbench-environment.sh
@@ -19,5 +19,11 @@ initializeWorkingDirAndScriptDir() {
 }
 initializeWorkingDirAndScriptDir
 
-${scriptDir}/git-clone-workbench.sh
-${scriptDir}/mvn-workbench.sh clean install
+mkdir "${scriptDir}/../logs"
+touch "${scriptDir}/../logs/git-clone-workbench.log"
+touch "${scriptDir}/../logs/git-workbench.log"
+touch "${scriptDir}/../logs/mvn-workbench.log"
+
+${scriptDir}/git-clone-workbench.sh | tee -a "${scriptDir}/../logs/git-clone-workbench.log"
+${scriptDir}/git-workbench.sh status | tee -a "${scriptDir}/../logs/git-workbench.log"
+${scriptDir}/mvn-workbench.sh clean install | tee -a "${scriptDir}/../logs/mvn-workbench.log"
