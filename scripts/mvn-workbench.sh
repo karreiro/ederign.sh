9a0484b8523097acfaba254607adae3a56a1bfc4
Paulo Martins <paulovmr@gmail.com>
2015-12-30 14:43:54 -0200

    Isolating common method on utils.sh.

--- scripts/mvn-workbench.sh
+++ scripts/mvn-workbench.sh
@@ -2,23 +2,8 @@
 
 # Run a mvn command on all workbench repositories.
 
-initializeWorkingDirAndScriptDir() {
-    # Set working directory and remove all symbolic links
-    workingDir=`pwd -P`
+source "$(dirname $0)/utils.sh"
 
-    # Go the script directory
-    cd `dirname $0`
-    # If the file itself is a symbolic link (ignoring parent directory links), then follow that link recursively
-    # Note that scriptDir=`pwd -P` does not do that and cannot cope with a link directly to the file
-    scriptFileBasename=`basename $0`
-    while [ -L "$scriptFileBasename" ] ; do
-        scriptFileBasename=`readlink $scriptFileBasename` # Follow the link
-        cd `dirname $scriptFileBasename`
-        scriptFileBasename=`basename $scriptFileBasename`
-    done
-    # Set script directory and remove other symbolic links (parent directory links)
-    scriptDir=`pwd -P`
-}
 initializeWorkingDirAndScriptDir
 workbenchRepositoriesDir="$scriptDir/../.."
 
